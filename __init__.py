18e4faade01b167a20c7f978d14bc962bde27ba4
Joe Sanford <joe@cs.tufts.edu>
2015-03-16 15:39:05 -0400

    About, resume, and BT pages in

--- __init__.py
+++ __init__.py
@@ -16,8 +16,24 @@ def about():
 def contact():
     return render_template('contact.html')
 
-@app.route('/single-project.html')
-def project():
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/homeauto')
+def homeauto():
+    return render_template('homeauto.html')
+
+@app.route('/guitarpp')
+def guitarpp():
+    return render_template('guitarpp.html')
+
+@app.route('/blockytalky')
+def blockytalky():
+    return render_template('blockytalky.html')
+
+@app.route('/projects')
+def projects():
     return render_template('single-project.html')
 
 if __name__ == '__main__':
