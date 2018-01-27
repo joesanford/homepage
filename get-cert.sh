0deff8f27ba9f3adc42b801a716605d74c034a3a
Joe Sanford <josephsanford@gmail.com>
2018-01-27 15:30:59 -0500

    Changed from pdfjs to images

--- get-cert.sh
+++ get-cert.sh
@@ -1,7 +1,1 @@
-docker run -it --rm \
-      -v certs:/etc/letsencrypt \
-      -v certs-data:/data/letsencrypt \
-      deliverous/certbot \
-      certonly \
-      --webroot --webroot-path=/data/letsencrypt \
-      -d jpsanford.com -d www.jpsanford.com
+certbot certonly --agree-tos -m josephsanford@gmail.com -d jpsanford.com -d www.jpsanford.com --dns-digitalocean --dns-digitalocean-credentials=credentials.ini --noninteractive
